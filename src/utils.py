11e30aa644e7abf16655cc4e997d8ae8621ed7cc
mmackowsky <maciostah@gmail.com>
2024-03-20 20:26:48 +0100

    feat(utils): add VerifyToken class

--- src/utils.py
+++ src/utils.py
@@ -18,3 +18,15 @@ class UnauthenticatedException(HTTPException):
         super().__init__(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Requires authentication"
         )
+
+
+class VerifyToken:
+"""Does all the token verification using PyJWT"""
+
+def __init__(self):
+        self.config = get_settings()
+
+        # This gets the JWKS from a given URL and does processing, so you can
+        # use any of the keys available
+        jwks_url = f'https://{self.config.auth0_domain}/.well-known/jwks.json'
+        self.jwks_client = jwt.PyJWKClient(jwks_url)
