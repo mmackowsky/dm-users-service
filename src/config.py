a1b223f20473b09f313680b37309a8c29e92779f
mmackowsky <maciostah@gmail.com>
2024-03-21 18:53:06 +0100

    feat(config): update

--- src/config.py
+++ src/config.py
@@ -1,13 +1,20 @@
+import os
 from functools import lru_cache
 
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    auth0_domain: str
-    auth0_api_audience: str
-    auth0_issuer: str
-    auth0_algorithms: str
+    auth0_client_id: str = os.getenv("AUTH0_CLIENT_ID")
+    auth0_client_secret: str = os.getenv("AUTH0_CLIENT_SECRET")
+    auth0_domain: str = os.getenv("AUTH0_DOMAIN")
+    app_secret_key: str = os.getenv("APP_SECRET_KEY")
+    auth0_api_audience: str = os.getenv("AUTH0_API_AUDIENCE", "")
+    auth0_issuer: str = os.getenv("AUTH0_ISSUER")
+    auth0_algorithms: str = os.getenv("AUTH0_ALGORITHMS")
 
     class Config:
         env_file = ".env"
