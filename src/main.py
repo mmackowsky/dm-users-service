5d1136bececb485e4ff9e66be8db60e5194bfcea
mmackowsky <maciostah@gmail.com>
2024-03-29 21:10:31 +0100

    feat(main): update

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import requests
+import uvicorn
 from fastapi import FastAPI, Security
 
 from utils import VerifyToken
@@ -9,7 +11,7 @@ auth = VerifyToken()
 @app.get("/api/public")
 def public():
     """No access token required to access this route"""
-
+    data = requests.get(url="http://127.0.0.1:8001/api/public")
     result = {
         "status": "success",
         "msg": (
@@ -17,10 +19,14 @@ def public():
             "authenticated to see this."
         ),
     }
-    return result
+    return result, data
 
 
 @app.get("/api/private")
 def private(auth_result: str = Security(auth.verify)):
     """A valid access token is required to access this route"""
     return auth_result
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8002)
