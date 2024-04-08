46e186acb116588eb5f9449f45f5c63303ced758
mmackowsky <maciostah@gmail.com>
2024-04-08 19:37:26 +0200

    config: add settings for db connect

--- src/config.py
+++ src/config.py
@@ -17,7 +17,12 @@ class Settings(BaseSettings):
     AUTH0_API_AUDIENCE: str = os.getenv("AUTH0_API_AUDIENCE", "")
     AUTH0_ISSUER: str = os.getenv("AUTH0_ISSUER")
     AUTH0_ALGORITHMS: str = os.getenv("AUTH0_ALGORITHMS")
-    SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL")
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
+    SQLALCHEMY_DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     class Config:
         env_file = ".env"
