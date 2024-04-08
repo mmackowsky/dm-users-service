96599257ede2124d9d2a2a4a1c27988d266553da
mmackowsky <maciostah@gmail.com>
2024-04-08 19:38:32 +0200

    feat(datastructures): update

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,7 +1,10 @@
+from pydantic import BaseModel
+from sqlalchemy import TIMESTAMP, Boolean, Column, ForeignKey, Integer, String, text
+
 from database import Base
 
 
-class UsernamePasswordForm(Base):
+class UsernamePasswordForm(BaseModel):
     username: str
     password: str
 
@@ -12,7 +15,7 @@ class UserForm(UsernamePasswordForm):
     user_type: str
 
 
-class UserUpdateForm(Base):
+class UserUpdateForm(BaseModel):
     username: str
     email: str = None
     full_name: str = None
@@ -20,10 +23,12 @@ class UserUpdateForm(Base):
 
 
 class User(Base):
-    id: int
-    username: str
-    email: str = None
-    full_name: str = None
-    user_type: str
-    hashed_password: str
-    created_by: int
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=True)
+    full_name = Column(String(150), unique=False, nullable=True)
+    user_type = Column(String(50), unique=False, nullable=False)
+    hashed_password = Column(String(200), unique=True, nullable=False)
+    created_by = Column(ForeignKey(id), nullable=False)
