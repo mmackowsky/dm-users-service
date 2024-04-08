e245bfe497547f72db59f6390f5d0712880a6359
mmackowsky <maciostah@gmail.com>
2024-04-08 22:07:34 +0200

    feat(schemas): add file

---
+++ src/schemas.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+
+class UsernamePasswordForm(BaseModel):
+    username: str
+    password: str
+
+
+class UserForm(UsernamePasswordForm):
+    email: str = None
+    full_name: str = None
+    user_type: str
+
+
+class UserUpdateForm(BaseModel):
+    username: str
+    email: str = None
+    full_name: str = None
+    user_type: str = None
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    email: str = None
+    full_name: str = None
+    user_type: str
+    hashed_password: str
+
+    class Config:
+        orm_mode = True
