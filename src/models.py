535c3ed4eeb360ddf860a012549cc8ca4f5a3179
mmackowsky <maciostah@gmail.com>
2024-04-08 21:55:08 +0200

    rename: datastructures -> models.py

--- src/models.py
+++ src/models.py
@@ -31,4 +31,4 @@ class User(Base):
     full_name = Column(String(150), unique=False, nullable=True)
     user_type = Column(String(50), unique=False, nullable=False)
     hashed_password = Column(String(200), unique=True, nullable=False)
-    created_by = Column(ForeignKey(id), nullable=False)
+
