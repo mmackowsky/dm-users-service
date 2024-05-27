ce062abc9d8437a5f2482c812f2bcf7e9412fdc9
mmackowsky <maciostah@gmail.com>
2024-05-27 19:12:54 +0200

    feat(models): update

--- src/models.py
+++ src/models.py
@@ -12,3 +12,6 @@ class User(Base):
     full_name = Column(String(150), unique=False, nullable=True)
     user_type = Column(String(50), unique=False, nullable=False)
     hashed_password = Column(String(200), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"<User(username={self.username}, email={self.email}, hashed_password={self.hashed_password})>"
